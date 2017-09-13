718017cde4a8709099054f3614938c1c878d844b
Sara Bilmes <saraesrig@gmail.com>
2017-09-13 22:54:20 +0300

    Seeding data - error

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+u = User.create(
+email: 'joe@user.com',
+password: 'password',
+confirmed_at: Time.now
+)
+
+15.times do
+  User.create!(
+  email: Faker::Internet.email,
+  password: Faker::Internet.password,
+  confirmed_at: Time.now
+  )
+end
+users = User.all
+
+50.times do
+  # #1
+  Item.create!(
+  name:  Faker::Lorem.sentence,
+  user: users.sample
+  )
+end
+items = Item.all
+
+puts "Seed finished"
+puts "#{Item.count} wikis created"
+puts "#{User.count} users created"
